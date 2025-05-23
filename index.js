78f5a7481424e638e15b4a59404bcb0580d88f64
Taranpreet Singh <taranpreet2468@gmail.com>
2025-05-23 19:03:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -30,42 +30,92 @@ async function checkWeather(city) {
             weatherIcon.src = "/Assets/Cloud Weather Icon.png";
             weatherIcon.alt = "Cloudy Weather";
             document.body.style.backgroundImage = "url('Assets/Cloudbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
             break;
         case "Clear":
             weatherIcon.src = "/Assets/Clear Weather icon.png";
             weatherIcon.alt = "Clear Sky";
             document.body.style.backgroundImage = "url('Assets/clearbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
             break;
         case "Drizzle":
             weatherIcon.src = "/Assets/Drizzle Weather icon.png";
             weatherIcon.alt = "Drizzle";
             document.body.style.backgroundImage = "url('Assets/Drizzlebg.png')";
+            document.querySelector(".condition").textContent = condition;
             break;
         case "Mist":
             weatherIcon.src = "/Assets/Mist Icon.png";
             weatherIcon.alt = "Misty";
             document.body.style.backgroundImage = "url('Assets/Mistbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
             break;
         case "Rain":
             weatherIcon.src = "/Assets/Rain Icon.png";
             weatherIcon.alt = "Rainy Weather";
             document.body.style.backgroundImage = "url('Assets/Rainbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
             break;
         case "Snow":
             weatherIcon.src = "/Assets/Snow Icon.png";
             weatherIcon.alt = "Snowy Weather";
             document.body.style.backgroundImage = "url('Assets/Snowbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
             break;
-			case "Thunderstorm":
+		case "Thunderstorm":
             weatherIcon.src = "/Assets/Thunderstorm.png";
             weatherIcon.alt = "Thunderstorm Weather";
             document.body.style.backgroundImage = "url('Assets/Thunderstormbg.jpg')";
-            break;
-			case "Haze":
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Haze":
             weatherIcon.src = "/Assets/Haze.png";
             weatherIcon.alt = "Haze Weather";
             document.body.style.backgroundImage = "url('Assets/Hazebg.jpg')";
-            break;
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Dust":
+            weatherIcon.src = "/Assets/DustIcon.png";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/Dustbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Smoke":
+            weatherIcon.src = "/Assets/SmokeIcon.png";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/Smokebg.jpg')";
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Fog":
+            weatherIcon.src = "/Assets/FogIcon.jpg";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/Fogbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Sand":
+            weatherIcon.src = "/Assets/SandIcon.png";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/SandBg.jpg')";
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Ash":
+            weatherIcon.src = "/Assets/AshIcon.png";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/Ashbg.avif')";
+            document.querySelector(".condition").textContent = condition;
+        break;
+		case "Squall":
+            weatherIcon.src = "/Assets/SquallICon.png";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/Squallbg.jpg')";
+            document.querySelector(".condition").textContent = condition;
+        break;
+        case "Tornado":
+            weatherIcon.src = "/Assets/TornadoIcon.png";
+            weatherIcon.alt = "Dust Weather";
+            document.body.style.backgroundImage = "url('Assets/TornadoBg.jpg')";
+            document.querySelector(".condition").textContent = condition;
+        break;
         default:
             weatherIcon.src = "/Assets/Weather Default Icon.png";
             weatherIcon.alt = "Weather Condition";
